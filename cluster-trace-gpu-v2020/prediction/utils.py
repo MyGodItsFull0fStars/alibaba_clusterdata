55101415221f7c0c0e0368994888667c402adc47
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-08-02 21:13:20 +0200

    Added content for prediction

--- cluster-trace-gpu-v2020/prediction/utils.py
+++ cluster-trace-gpu-v2020/prediction/utils.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
-def get_df(file: str, header=None):
-    df = pd.read_csv(file, header=None)
-    # df.columns = DF_HEADER.get(key, df.columns)
+def get_df(file: str, header=None, sample: bool = False, sample_number: int = 1000):
+    if sample:
+        df = pd.read_csv(file, header=None).sample(sample_number)
+    else:
+        df = pd.read_csv(file, header=None)
+    
     df.columns = pd.read_csv("{}.header".format(
         file.split('.csv')[0])).columns if header is None else header
     return df
