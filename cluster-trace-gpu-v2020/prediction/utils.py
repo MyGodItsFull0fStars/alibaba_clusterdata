00fd9eb56d1ebbb9de1e78929ebc30ff53d06802
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-11-23 14:07:15 +0100

    Added cuda check to utils.py

--- cluster-trace-gpu-v2020/prediction/utils.py
+++ cluster-trace-gpu-v2020/prediction/utils.py
@@ -1,17 +1,19 @@
 import math
+from typing import List
 
 import numpy as np
 import pandas as pd
 import torch
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 
+import nvidia_smi
 
 def get_df(file: str, header=None, sample: bool = False, sample_number: int = 1000):
     if sample:
         df = pd.read_csv(file, header=None).sample(sample_number)
     else:
         df = pd.read_csv(file, header=None)
-    
+
     df.columns = pd.read_csv("{}.header".format(
         file.split('.csv')[0])).columns if header is None else header
     return df
@@ -20,9 +22,9 @@ def get_df(file: str, header=None, sample: bool = False, sample_number: int = 10
 def get_device() -> torch.device:
     return torch.device(get_device_as_string())
 
+
 def get_device_as_string() -> str:
     return 'cuda' if torch.cuda.is_available() else 'cpu'
-
 
 
 def get_rmse(actual_values, predicted_values) -> float:
@@ -31,10 +33,33 @@ def get_rmse(actual_values, predicted_values) -> float:
 
 
 def get_mape(actual_values, predicted_values):
-    '''returns the mean absolue percentage error'''
+    '''returns the mean absolute percentage error'''
     return np.mean(np.abs(actual_values - predicted_values) / np.abs(actual_values) * 100)
 
 
 def get_mae(actual_values, predicted_values) -> float:
     '''returns the mean absolute error'''
     return mean_absolute_error(actual_values, predicted_values)
+
+
+def get_available_cuda_devices(free_mem_threshold: float = 0.90) -> List[str]:
+    available_gpus: List[str] = []
+    if get_device_as_string() == 'cpu':
+        print('No CUDA device available')
+    
+    elif get_device_as_string() == 'cuda':
+        nvidia_smi.nvmlInit()
+        device_count = nvidia_smi.nvmlDeviceGetCount()
+        
+        for idx in range(device_count):
+            handle = nvidia_smi.nvmlDeviceGetHandleByIndex(idx)
+            info = nvidia_smi.nvmlDeviceGetMemoryInfo(handle)
+            
+            free_memory: float = info.free / info.total
+            
+            if free_memory >= free_mem_threshold:
+                available_gpus.append(f'cuda:{idx}')
+                
+        nvidia_smi.nvmlShutdown()
+    return available_gpus
+                
