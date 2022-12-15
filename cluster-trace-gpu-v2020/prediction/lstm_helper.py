d7d201397450dcfa70c3300be0efbe3fb2c67f91
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-12-15 15:06:35 +0100

    Removed files

--- cluster-trace-gpu-v2020/prediction/lstm_helper.py
+++
@@ -1,9 +0,0 @@
-from typing import Dict, List
-import pandas as pd
-import numpy as np
-
-
-class Machine():
-    
-    def __init__(self, machine_name: str) -> None:
-        self.machine_name: str = machine_name
