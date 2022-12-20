b566c453ab960a9a6dbb5592f4925d1ba6ea735c
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2022-12-20 17:15:15 +0100

    changed config for prediction

--- cluster-trace-gpu-v2020/prediction/lstm_timestamp_prediction.py
+++ cluster-trace-gpu-v2020/prediction/lstm_timestamp_prediction.py
@@ -25,7 +25,7 @@ from yaml.loader import SafeLoader
 
 # %%
 # Open the file and load the file
-with open('./model_configs/tasks_vs_no_tasks/utilization_with_tasks.yaml') as f:
+with open('./model_configs/tasks_vs_no_tasks/utilization_no_tasks.yaml') as f:
     yaml_config = yaml.load(f, Loader=SafeLoader)
     print(yaml_config)
 
