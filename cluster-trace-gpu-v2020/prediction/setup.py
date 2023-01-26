ae6969e42d765002e4f3f0885689f1e5467257f1
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-01-26 13:09:12 +0100

    Added setup files for jupyter imports

---
+++ cluster-trace-gpu-v2020/prediction/setup.py
@@ -0,0 +1,5 @@
+from setuptools import setup, find_packages
+setup(
+    name = 'prediction',
+    packages = find_packages(),
+)
---
+++ cluster-trace-gpu-v2020/prediction/setup_python_imports.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+conda activate ml_pytorch
+
+python3 setup.py install --user
