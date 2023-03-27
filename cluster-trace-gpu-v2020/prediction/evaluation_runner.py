63abe3891930b6695b988832ed48de5a897dd151
MyGodItsFull0fStars <chrbauer@edu.aau.at>
2023-03-27 20:40:19 +0200

    Added evaluation script

---
+++ cluster-trace-gpu-v2020/prediction/evaluation_runner.py
@@ -0,0 +1,26 @@
+# %%
+from os import walk
+import subprocess
+
+# %%
+model_configuration_path: str = './model_configs'
+
+# %%
+config_files: list[str] = list()
+
+# %%
+for (dir_path, dir_names, file_names) in walk(model_configuration_path):
+    print(dir_path, dir_names, file_names)
+    file_names = [f'{dir_path}/{file}' for file in file_names]
+    config_files.extend(file_names)
+
+
+# %%
+for config in config_files:
+    subprocess.run(['python', 'lstm_timestamp_prediction.py', f'--config={config}', '--dry_run=True'])
+    print(f'finished {config}')
+
+# %%
+print('done')
+
+
